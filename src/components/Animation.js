be8b5f40f701c1c2d8f32db48b5902edbf1c6cc2
Kristian Pedersen <kp@Kristians-iMac.net>
2020-11-22 12:16:44 +0100

    SVG lines scale properly on window resize

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,81 +1,102 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 
 function pause(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 function Animation({ animationSpeed, animationSpeedRef, board }) {
+	const initialRender = useRef(true)
+
+	useEffect(function resizeSVGOnWindowResize() {
+		if (initialRender.current) {
+			console.log("Setting up event listener")
+			window.addEventListener("resize", function handleResize() {
+				const newBoardHeight = document.querySelector(".board").getBoundingClientRect().height
+				document.querySelectorAll("svg").forEach(svg => svg.setAttribute("height", newBoardHeight))
+			})
+		}
+	})
+
 	useEffect(function animateBoardOnStateChange() {
-		const buttons = [...document.querySelectorAll(".board-button")]
-		document.querySelectorAll("svg").forEach(svg => svg.remove())
+		const boardDimensions = document.querySelector(".board").getBoundingClientRect()
+		const boardWidth = boardDimensions.width
+		const boardHeight = boardDimensions.height
 
-		async function colorizeButtons() {
-			for (const [index, move] of board.entries()) {
-				// Initialize svg
-				const svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg')
-				svg.style.position = "absolute"
-				svg.style.left = 0
-				svg.style.top = 0
-				svg.setAttribute("viewBox", "0 0 400 800")
-				let pathString = ""
-				svg.setAttributeNS(null, 'width', '100vw')
-				svg.setAttributeNS(null, 'height', '100%')
-				const blackLine = document.createElementNS("http://www.w3.org/2000/svg", "path")
-				const coloredLine = document.createElementNS("http://www.w3.org/2000/svg", "path")
-
-				const button = buttons.find(function getCurrentButton(b) {
-					return b.innerHTML === move.name
-				})
-				const hue = Math.floor(index * (270 / board.length))
-
-				if (index > 0) { // ... we draw a line from previous move to current move
-					const { left, width, top, height } = button.getBoundingClientRect()
-					const x = left + width / 2
-					const y = top + height / 2
-					const previousButton = buttons.find(b => b.innerHTML === board[index - 1].name)
-					const previousClientRect = previousButton.getBoundingClientRect()
-					const previous = {
-						left: previousClientRect.left,
-						width: previousClientRect.width,
-						top: previousClientRect.top,
-						height: previousClientRect.height,
-					}
-					const previousX = previous.left + previous.width / 2
-					const previousY = previous.top + previous.height / 2
-
-					pathString += `M ${x} ${y} L ${previousX} ${previousY}`
-
-					blackLine.setAttributeNS(null, "d", pathString);
-					blackLine.setAttributeNS(null, 'stroke', "black")
-					blackLine.setAttributeNS(null, 'stroke-width', "4")
-					blackLine.setAttributeNS(null, 'stroke-opacity', 0.5)
-
-					coloredLine.setAttributeNS(null, "d", pathString);
-					coloredLine.setAttributeNS(null, 'stroke', `hsl(${hue}, 100%, 50%)`)
-					coloredLine.setAttributeNS(null, 'stroke-width', "2")
-					// coloredLine.setAttributeNS(null, 'stroke-opacity', 0.5)
-
-					svg.appendChild(blackLine)
-					svg.appendChild(coloredLine)
-
-					svg.style.position = "absolute"
+		if (initialRender.current) {
+			initialRender.current = false
+		} else if (board.length >= 25) {
+			const buttons = [...document.querySelectorAll(".board-button")]
+			document.querySelectorAll("svg").forEach(svg => svg.remove())
+			async function colorizeButtons() {
+				for (const [index, move] of board.entries()) {
+					// Initialize svg
+					const svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg')
+					svg.setAttribute("preserveAspectRatio", "none")
+					svg.setAttribute("viewBox", `0 0 ${boardWidth} ${boardHeight}`)
+					svg.style.position = "relative"
 					svg.style.left = 0
 					svg.style.bottom = 0
-					document.body.appendChild(svg)
-				}
 
-				if (button !== undefined) {
-					button.classList.add("active")
-					if (index === 0) {
-						button.style.borderColor = "white"
+					const button = buttons.find(function getCurrentButton(b) {
+						return b.innerHTML === move.name
+					})
+					let pathString = ""
+					svg.setAttributeNS(null, 'width', '100vw')
+					svg.setAttributeNS(null, 'height', document.querySelector(".board").getBoundingClientRect().height)
+					const blackLine = document.createElementNS("http://www.w3.org/2000/svg", "path")
+					const coloredLine = document.createElementNS("http://www.w3.org/2000/svg", "path")
+
+					const hue = Math.floor(index * (270 / board.length))
+
+					if (index > 0) { // ... we draw a line from previous move to current move
+						const { left, width, top, height } = button.getBoundingClientRect()
+						const x = left + width / 2
+						const y = Math.abs(top - document.querySelector(".board").getBoundingClientRect().top + height / 2)
+						const previousButton = buttons.find(b => b.innerHTML === board[index - 1].name)
+						const previousClientRect = previousButton.getBoundingClientRect()
+						const previous = {
+							left: previousClientRect.left,
+							width: previousClientRect.width,
+							top: previousClientRect.top,
+							height: previousClientRect.height,
+						}
+						const previousX = previous.left + width / 2
+						const previousY = Math.abs(previous.top - document.querySelector(".board").getBoundingClientRect().top + height / 2)
+
+						pathString += `M ${x} ${y} L ${previousX} ${previousY}`
+
+						blackLine.setAttributeNS(null, "d", pathString);
+						blackLine.setAttributeNS(null, 'stroke', "black")
+						blackLine.setAttributeNS(null, 'stroke-width', "4")
+						blackLine.setAttributeNS(null, 'stroke-opacity', 0.5)
+
+						coloredLine.setAttributeNS(null, "d", pathString);
+						coloredLine.setAttributeNS(null, 'stroke', `hsl(${hue}, 100%, 50%)`)
+						coloredLine.setAttributeNS(null, 'stroke-width', "2")
+						// coloredLine.setAttributeNS(null, 'stroke-opacity', 0.5)
+
+						svg.appendChild(blackLine)
+						svg.appendChild(coloredLine)
+
+						svg.style.position = "absolute"
+						svg.style.left = 0
+						svg.style.bottom = 0
+						document.querySelector(".board").appendChild(svg)
 					}
-					button.style.backgroundColor = `hsl(${hue}, 50%, 80%)`
-					button.style.color = "black"
+
+					if (button !== undefined) {
+						button.classList.add("active")
+						if (index === 0) {
+							button.style.borderColor = "white"
+						}
+						button.style.backgroundColor = `hsl(${hue}, 50%, 80%)`
+						button.style.color = "black"
+					}
+					await pause(animationSpeedRef.current)
 				}
-				await pause(animationSpeedRef.current)
 			}
+			colorizeButtons()
 		}
-		colorizeButtons()
 	}, [board])
 	return null
 }
